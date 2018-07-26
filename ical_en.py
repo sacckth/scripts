0f1e1a02f0492c373c280dd1a94112b396aba3d6
sacckth <sergioachavez@gmail.com>
2018-07-26 23:06:22 +0200

    update

--- ical_en.py
+++ ical_en.py
@@ -1,97 +1,95 @@
 #!/usr/bin/env python3
 
-def createEvent (cal,eventData):
+def createEvent (cal,args):
     from datetime import datetime
     from icalendar import Event, vCalAddress, vText, vDatetime
     import pytz
-    event = Event()
-    if eventData["summary"] == '':
-        eventData["summary"] = "Dummy event" 
-    print ("\tGenerating event <<%s>>...\n" % (eventData["summary"]))
-    bdate=[int(i) for i in eventData["begin_date"].split('-')]
-    bhour=[int(i) for i in eventData["begin_hour"].split(':')] 
-    edate=[int(i) for i in eventData["end_date"].split('-')]
-    ehour=[int(i) for i in eventData["end_hour"].split(':')]
-    btz = pytz.timezone(eventData["begin_timezone"])
-    etz = pytz.timezone(eventData["end_timezone"])
-    event["summary"]   = eventData["summary"]
-    event["organizer"] = vCalAddress('MAILTO:'+eventData["organizer"])
-    event["dtstart"]   = vDatetime(datetime(bdate[2],bdate[1],bdate[0],bhour[0],bhour[1],0,tzinfo=btz))
-    event["dtend"]     = vDatetime(datetime(edate[2],edate[1],edate[0],ehour[0],ehour[1],0,tzinfo=etz))
-    event["dtstamp"]   = vDatetime(datetime(bdate[2],bdate[1],bdate[0],bhour[0],bhour[1],0,tzinfo=btz))
-    event["location"]  = vText(eventData["location"])
-    if eventData["description"] == '':
-        event["description"] = vText("XoXo.")
-    else:
+    for i in range(0,args.nr_events):
+        print ("\tGenerating event %i of %i...\n" % (i+1,args.nr_events))
+        eventData = getEventData(args)
+        event = Event()
+        bdate=[int(i) for i in eventData["begin_date"].split('-')]
+        bhour=[int(i) for i in eventData["begin_hour"].split(':')] 
+        edate=[int(i) for i in eventData["end_date"].split('-')]
+        ehour=[int(i) for i in eventData["end_hour"].split(':')]
+        btz = pytz.timezone(eventData["begin_timezone"])
+        etz = pytz.timezone(eventData["end_timezone"])
+        event["summary"]   = eventData["summary"]
+        event["organizer"] = vCalAddress('MAILTO:'+eventData["organizer"])
+        event["dtstart"]   = vDatetime(datetime(bdate[2],bdate[1],bdate[0],bhour[0],bhour[1],0,tzinfo=btz))
+        event["dtend"]     = vDatetime(datetime(edate[2],edate[1],edate[0],ehour[0],ehour[1],0,tzinfo=etz))
+        event["dtstamp"]   = vDatetime(datetime(bdate[2],bdate[1],bdate[0],bhour[0],bhour[1],0,tzinfo=btz))
+        event["location"]  = vText(eventData["location"])
         event["description"] = vText(eventData["description"])
-    event['uid'] = "xOxO-"+str(datetime.timestamp(datetime.now()))+"-XoXo"
-    event['priority'] = 5
-    cal.add_component(event)
+        event['uid'] = "xOxO-"+str(datetime.timestamp(datetime.now()))+"-XoXo"
+        event['priority'] = 5
+        cal.add_component(event)
     return cal
 def displayInput(eventData) :
     correct = "n"
-    for key, value in eventData :
-        print (key,value)
-    correct =input ("Is this information correct? y/n: " )    
+    for key, value in eventData.items() :
+        print ("%s: %s" % (str(key),str(value)) )
+    correct = input ("Is this information correct? y/n: " )    
     return correct
 def getEventData(args):
     eventData = {} 
-    eventData["summary"]      = args.event
-    eventData["location"]     = args.location
-    eventData["description"]  = args.descr
-    eventData["organizer"]    = args.organizer
+    eventData["summary"]           = args.event
+    eventData["location"]          = args.location
+    eventData["description"]       = args.descr
+    eventData["organizer"]         = args.organizer
     eventData["begin_date"]        = args.b_date
     eventData["begin_hour"]        = args.b_hour
-    eventData["begin_timezone"]          = args.b_tz
-    eventData["end_date"]        = args.e_date
-    eventData["end_hour"]        = args.e_hour
-    eventData["end_timezone"]          = args.e_tz
+    eventData["begin_timezone"]    = args.b_tz
+    eventData["end_date"]          = args.e_date
+    eventData["end_hour"]          = args.e_hour
+    eventData["end_timezone"]      = args.e_tz
     if args.nr_events == 1 :
-       print ( 'Generanting %s...' % (args.event))
+       print ( 'Generanting event %s...' % (args.event))
        return eventData
     else :
-       for i in range(0,args.nr_events):
-            correct = "n"
-            while correct != 'y' :
-                print ( '\n====== Generating event %i of %i ======' % (i+1,args.nr_events))
-                eventData["summary"] = input("Event Summary: ")
-                loc = input("Give location, press ENTER to use default ("+eventData["location"]+")?: ")
-                if loc != '':
-                    eventData["location"] = loc
-                desc = input("Do you want to add a description? y/n: ")
-                if desc == "y":
-                    eventData["description"] = input("\tProvide a description: ")
-                bdate = input("Give a begin date DD-MM-AAAA, press ENTER to use today's date ("+args.b_date+"): ")
-                if bdate == '':
-                    eventData["begin_date"] = args.b_date
-                else : 
-                    eventData["begin_date"] = bdate
-                bhour = input("Give begin time HH:MM, press ENTER to use default ("+args.b_hour+"): ")
-                if bhour == '':
-                    eventData["begin_hour"] = args.b_hour
-                else :
-                    eventData["begin_hour"] = bhour
-                btz   = input("Give begin time zone, press ENTER to use default ("+args.b_tz+"): ")
-                if btz == '' :
-                    eventData["begin_timezone"] = args.b_tz
-                else :
-                    eventData["begin_timezone"] = btz
-                edate = input("Give end date DD-MM-AAAA, press ENTER to use same as begin date ("+eventData["begin_date"]+"): ")
-                if edate == '':
-                    eventData["end_date"] = eventData["begin_date"]
-                else :
-                    eventData["end_date"] = edate
-                ehour = input("Give end time HH:MM, press ENTER to use default ("+args.e_hour+"): ")
-                if ehour == '':
-                    eventData["end_hour"] = args.e_hour
-                else :
-                    eventData["end_hour"] = ehour
-                etz  = input("Give end timezone, press ENTER to use same as intial timezone ("+eventData["begin_timezone"]+"): ")
-                if etz == '' :
-                    eventData["end_timezone"] = eventData["begin_timezone"]
-                else :
-                    eventData["end_timezone"] = etz
-                correct = displayInput(eventData)
+        correct = "n"
+        while correct != 'y' :
+            print ( '\n===============================================')
+            eventData["summary"] = input("Event Summary: ")
+            loc = input("Give location, press ENTER to use default ("+eventData["location"]+")?: ")
+            if loc != '':
+                eventData["location"] = loc
+            desc = input("Do you want to add a description? y/n: ")
+            if desc == "y":
+                eventData["description"] = input("\tProvide a description: ")
+            else :
+                eventData["description"] = "XoXo."
+            bdate = input("Give a begin date DD-MM-AAAA, press ENTER to use today's date ("+args.b_date+"): ")
+            if bdate == '':
+                eventData["begin_date"] = args.b_date
+            else : 
+                eventData["begin_date"] = bdate
+            bhour = input("Give begin time HH:MM, press ENTER to use default ("+args.b_hour+"): ")
+            if bhour == '':
+                eventData["begin_hour"] = args.b_hour
+            else :
+                eventData["begin_hour"] = bhour
+            btz   = input("Give begin time zone, press ENTER to use default ("+args.b_tz+"): ")
+            if btz == '' :
+                eventData["begin_timezone"] = args.b_tz
+            else :
+                eventData["begin_timezone"] = btz
+            edate = input("Give end date DD-MM-AAAA, press ENTER to use same as begin date ("+eventData["begin_date"]+"): ")
+            if edate == '':
+                eventData["end_date"] = eventData["begin_date"]
+            else :
+                eventData["end_date"] = edate
+            ehour = input("Give end time HH:MM, press ENTER to use default ("+args.e_hour+"): ")
+            if ehour == '':
+                eventData["end_hour"] = args.e_hour
+            else :
+                eventData["end_hour"] = ehour
+            etz  = input("Give end timezone, press ENTER to use same as intial timezone ("+eventData["begin_timezone"]+"): ")
+            if etz == '' :
+                eventData["end_timezone"] = eventData["begin_timezone"]
+            else :
+                eventData["end_timezone"] = etz
+            correct = displayInput(eventData)
     return eventData
 
 def displayCal(cal):
@@ -104,7 +102,7 @@ if __name__ == "__main__":
     from datetime import datetime
     parser = argparse.ArgumentParser(description="Create .ics cal events")
     parser.add_argument("-e","--event", metavar='EVENT',
-                        help="Event summary",default='', dest='event')
+                        help="Event summary",default='Dummy Event', dest='event')
     parser.add_argument("-d","--description", metavar='DESCRIPTION',
                         help="Event description",default='', dest='descr')                     
     parser.add_argument("-B","--begin",metavar='DD-MM-YYYY',default=str(datetime.date(datetime.now()).strftime("%d-%m-%Y")),
@@ -129,8 +127,7 @@ if __name__ == "__main__":
     cal.add('prodid', '-//My calendar//Sergio//')
     cal.add('version', '1.0')
     cal.add('calscale',"GREGORIAN")
-    eventData = getEventData(args)
-    cal = createEvent(cal,eventData)
+    cal = createEvent(cal,args)
     f = open(args.filename, 'wb')    
     f.write(cal.to_ical())
     f.close()
